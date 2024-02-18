2d8ba1a12074c253892955fc9c34c7364770d178
Maksim Popov <exactlyy@gmail.com>
2024-02-18 04:41:20 +0000

    updated file after checking PI linter

--- run.py
+++ run.py
@@ -3,9 +3,9 @@
 
 import random  # generates random words for the game
 import string  # format letters to uppercase
-import sys # this allows to close the game if player don't want to play
+import sys  # this allows to close the game if player don't want to play
 
-# 7 drawings imported from module and represent lives in the game
+#  7 drawings imported from module and represent lives in the game
 
 from hangman import (
     hangman_1,
@@ -22,7 +22,7 @@ from book_2 import WORDS_2
 from book_3 import WORDS_3
 from book_4 import WORDS_4
 
-# code for game
+#  code for game
 
 
 def introduction():
@@ -30,10 +30,10 @@ def introduction():
     This section explains game rules to the player.
     """
     print("_"*70)
-    print(" "*8 +"*" * 30)
+    print(" " * 8 + "*" * 30)
     print("**********WELCOME TO THE HANGMAN GAME *********")
-    print(" "*8 +"*" * 30)
-    print(" "*8 +"*" * 30)
+    print(" " * 8 + "*" * 30)
+    print(" " * 8 + "*" * 30)
     print("                     RULES\n")
     print("1. Attention!! In this game you can select level of difficulty.")
     print("2. There is a random word will be chosen by computer.")
@@ -49,17 +49,17 @@ def introduction():
 def rhyme():
     """This function will ask player if he is ready to play"""
     while True:
-        response = input("Fancy to hear a rhyme about hangman? Press ('Y'/'N')\n").strip().upper()
+        response = input("Fancy a hangman rhyme?('Y'/'N')\n").strip().upper()
         if response == 'Y':
-            print(("_"* 80))
+            print(("_" * 80))
             print("*****      THE HANGMAN GAME RHYME                 *****")
 
             print("*****  In Hangman's play, a word concealed,       *****")
             print("*****  Guessing letters, fate's revealed.         *****")
             print("*****  Each wrong guess brings the gallows near,  *****")
             print("*****  But solve the puzzle, redemption's cheer.  *****")
-            print((" "*8 +"*" * 30))
-            print((" "*8 +"*" * 30))
+            print((" " * 8 + "*" * 30))
+            print((" " * 8 + "*" * 30))
             print("I hope you enjoyed it!")
             break
         elif response == 'N':
@@ -74,8 +74,8 @@ def library_choice():
     """
     Player picks the category for playing games
     """
-    library = {1: "book_1", 2: "book_2", 3: "book_3", 4: "book_4"}   
-    # Difficulty level
+    library = {1: "book_1", 2: "book_2", 3: "book_3", 4: "book_4"}
+    #  Difficulty level
     print("Select difficulty level:")
     print("1 - Easy")
     print("2 - Normal")
@@ -93,7 +93,7 @@ def library_choice():
             if difficulty_level == 1:
                 library_selection = WORDS_1
                 print("\n You selected difficulty Level: EASY")
-                break              
+                break
 
             elif difficulty_level == 2:
                 library_selection = WORDS_2
@@ -110,12 +110,12 @@ def library_choice():
                 print("\n You selected difficulty Level: Hard")
                 break
 
-            # IF statement picks random word from chosen library
+            #  IF statement picks random word from chosen library
             if library_selection:
                 random_word = random.choice(library_selection)
                 return random_word.upper()
 
-            else: 
+            else:
                 print("Incorrectly selected difficulty level")
                 print("Please select the difficulty level 1, 2, 3 or 4")
     return library_selection
@@ -127,8 +127,7 @@ def random_word():
     return word
 
 
-
-def hangman_picture (attempt):
+def hangman_picture(attempt):
     """Displays a hangman picture which depends on attempts left"""
     if attempt == 6:
         hangman_1()
@@ -143,7 +142,7 @@ def hangman_picture (attempt):
     elif attempt == 1:
         hangman_6()
     else:
-        hangman_7()  
+        hangman_7()
 
 
 def greetings_player(func):
@@ -155,15 +154,15 @@ def greetings_player(func):
 
     return inner
 
+
 def get_name():
     """Function to ask player his name"""
     while True:
         name = input("Please enter your name: ")
         if name.isalpha() and len(name) > 1:
-            return name.title() 
+            return name.title()
         else:
             print("Invalid value. Please enter your name again: ")
-        
 
 
 def ready_to_play():
@@ -183,21 +182,20 @@ def ready_to_play():
 
 def hidden_word():
     'This function displays hidden word and responds for the decreasing attempts'
-    # variables that I will use in this function
+    #  variables that I will use in this function
     the_word = random_word().upper()
     alphabet = set(string.ascii_uppercase)
-    the_word_letters = set(the_word.upper()) # letters from the_word
-    named_letters = set() # letters player used already
-    my_list = ["_"] * len(the_word) # the_word
-    attempt = 7 # Player has 7 attempts
+    the_word_letters = set(the_word.upper())  #  letters from the_word
+    named_letters = set()  # letters player used already
+    my_list = ["_"] * len(the_word)
+    attempt = 7  # Player has 7 attempts
 
-
-    while "_" in my_list and attempt > 0 :
+    while "_" in my_list and attempt > 0:
         print(" " * 40, " ".join(my_list))
         new_letter = input("Please enter a letter!\n").upper().strip()
 
         if new_letter in alphabet - named_letters:
-            named_letters.add(new_letter) # add new letter to the list
+            named_letters.add(new_letter)  # add new letter to the list
 
             if new_letter in the_word_letters:
                 the_word_letters.remove(new_letter)
@@ -205,10 +203,10 @@ def hidden_word():
 
             elif new_letter not in the_word_letters:
                 print(f"Alas! This is incorrect! This word has no letter: '{new_letter}'")
-                attempt -=1
-                hangman_picture(attempt) # this should display the hangman picture
-                    
-                if attempt == 0: # This checks after each answer if player has attempts
+                attempt - =1
+                hangman_picture(attempt)  # this displays the hangman picture
+
+                if attempt == 0:  # This checks if player has attempts
                     print(f" Sorry. No '{new_letter}' in this word ! You have no more attempts! You lost")
                     print("The word you tried to guess was: ", the_word)
                     print(("_") * 70)
@@ -227,21 +225,20 @@ def hidden_word():
                             return False
                         else:
                             print("Sorry, I didn't get this, please enter 'Y' or 'N'")
-            
+
             for i, letter in enumerate(the_word):
-                #print("loop ", letter, new_letter)
                 if letter == new_letter:
                     my_list[i] = new_letter
-                    
+
         elif new_letter in named_letters:
-                print("You tried this letter before. Please enter another letter!")
+            print("You tried this letter before. Please enter another letter!")
         else:
-                ("Invalid value, please enter another letter!")
+            ("Invalid value, please enter another letter!")
         print("You have left attempts:", attempt)
         print("You used next letters:", ", " .join(named_letters))
     print("\n You won! The word is:", " ".join(my_list))
     print(" ")
-    print(" "*18 +"*" * 30)
+    print(" " * 18 + "*" * 30)
     while True:
         restart = input("Would you like to restart and try again? Y / N\n").strip().upper()
 
@@ -251,7 +248,6 @@ def hidden_word():
             return False
         else:
             print("Sorry, I didn't get this, please enter 'Y' or 'N'")
-
 
 
 def main():
@@ -266,5 +262,4 @@ def main():
             break
 
 
-
-main()  
+main()
